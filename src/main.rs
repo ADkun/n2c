f6426abe0432f11253f95a73162c94ba16017f0b
ADkun <adkun@github.com>
2024-10-31 16:18:45 +0800

    fix: more precise output

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,9 @@ fn main() {
 fn get_human_readable_size(mut size: f64) -> String {
     let units = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
     let mut unit_index = 0;
-    while size > 1024.0 && unit_index < units.len() - 1 {
+    while size >= 1024.0 && unit_index < units.len() - 1 {
         size /= 1024.0;
         unit_index += 1;
     }
-    format!("{:.2} {}", size, units[unit_index])
+    format!("{:.5} {}", size, units[unit_index])
 }
